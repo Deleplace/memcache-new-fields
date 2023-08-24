242c1948772f09faa15567394350418bf73b8678
Valentin Deleplace <deleplace2015@gmail.com>
2023-08-24 16:48:14 +0200

    More tests (Go 1.20, v2).

--- go1.20/use_memcache.go
+++ go1.20/use_memcache.go
@@ -16,7 +16,7 @@ func useMemcache(ctx context.Context, w io.Writer) {
 		if ok {
 			fmt.Fprintln(w, "✓")
 		} else {
-			fmt.Fprintf(w, "✖ Failed: "+errmsg, args...)
+			fmt.Fprintf(w, "✖ Failed: "+errmsg+"\n", args...)
 		}
 	}
 
@@ -32,8 +32,31 @@ func useMemcache(ctx context.Context, w io.Writer) {
 		key := "NON-EXISTENT"
 		fmt.Fprintf(w, "memcache.Get(ctx, %q)...\n", key)
 		item, err := memcache.Get(ctx, key)
-		assertf(err == memcache.ErrCacheMiss, "expected nil error, got %v", err)
+		assertf(err == memcache.ErrCacheMiss, "expected ErrCacheMiss, got %v", err)
 		assertf(item == nil, "expected nil item, got %v", item)
+	}
+
+	{
+		// TRADITIONAL USAGE (EXISTING API)
+
+		// What happens when setting/getting an empty value?
+		// Set succeeds, Get succeeds, with an empty item value.
+		key := "EMPTY"
+
+		value := []byte{} // empty
+		item := &memcache.Item{
+			Key:   key,
+			Value: value,
+		}
+		fmt.Fprintf(w, "\nmemcache.Set(ctx, %v)...\n", item)
+		err := memcache.Set(ctx, item)
+		assertf(err == nil, "expected nil error, got %v", err)
+
+		fmt.Fprintf(w, "memcache.Get(ctx, %q)...\n", key)
+		item, err = memcache.Get(ctx, key)
+		assertf(err == nil, "expected nil error, got %v", err)
+		assertf(item != nil, "expected non-nil item")
+		assertf(len(item.Value) == 0, "expected empty value, got %q", string(item.Value))
 	}
 
 	{
@@ -43,7 +66,7 @@ func useMemcache(ctx context.Context, w io.Writer) {
 
 		// Cleanup, because previous run may have set it
 		err := memcache.Delete(ctx, key)
-		if err != nil {
+		if err != nil && err != memcache.ErrCacheMiss {
 			fmt.Fprintf(w, "Error deleting entry for key %q: %v\n", key, err)
 		}
 
@@ -63,7 +86,7 @@ func useMemcache(ctx context.Context, w io.Writer) {
 	}
 
 	{
-		// NORMAL USAGE, BUT CHECK THE NEW FIELDS RETURNED (DEFAULT VALUES)
+		// TRADITIONAL USAGE, BUT CHECK THE NEW FIELDS RETURNED (DEFAULT VALUES)
 
 		key := "mykey"
 		expectedValue := []byte("foobar")
@@ -137,6 +160,11 @@ func useMemcache(ctx context.Context, w io.Writer) {
 	}
 
 	{
+		// DELETE LOCK
+		// this feature (included in the proto) is not available in the Go lib exported API.
+	}
+
+	{
 		// NORMAL USAGE AGAIN
 		key := "mykey"
 
@@ -152,7 +180,87 @@ func useMemcache(ctx context.Context, w io.Writer) {
 		}
 	}
 
+	{
+		// NEW API USAGE: PEEK MULTIPLE ITEMS
+
+		key1 := "mykey1"
+		value1 := []byte("foobar1")
+		item1 := &memcache.Item{
+			Key:   key1,
+			Value: value1,
+			// No Expiration
+		}
+
+		key2 := "mykey2"
+		value2 := []byte("foobar2")
+		item2 := &memcache.Item{
+			Key:        key2,
+			Value:      value2,
+			Expiration: 5 * time.Minute,
+		}
+
+		items := []*memcache.Item{item1, item2}
+
+		fmt.Fprintf(w, "\nmemcache.SetMulti(ctx, %d items)...\n", len(items))
+		err := memcache.SetMulti(ctx, items)
+		assertf(err == nil, "expected nil error, got %v", err)
+
+		keys := []string{key1, key2}
+
+		fmt.Fprintf(w, "\nmemcache.GetMulti(ctx, %q)...\n", keys)
+		itemsmap, err := memcache.GetMulti(ctx, keys)
+		assertf(err == nil, "expected nil error, got %v", err)
+		assertf(len(itemsmap) == len(keys), "expected %d entries, got a map with %d entries", len(keys), len(itemsmap))
+
+		item1in := itemsmap[key1]
+		assertf(item1in != nil, "entry %q not found in %v", key1, itemsmap)
+		if item1in != nil {
+			assertf(bytes.Equal(value1, item1in.Value), "expected item with value %q, got %q", string(value1), string(item1in.Value))
+			assertf(item1in.Timestamps.Expiration == nil, "expected nil Expiration timestamp, got %v", item1in.Timestamps.Expiration)
+			assertf(item1in.Timestamps.LastAccess == nil, "expected nil LastAccess timestamp, got %v", item1in.Timestamps.LastAccess)
+		}
+
+		item2in := itemsmap[key2]
+		assertf(item2in != nil, "entry %q not found in %v", key2, itemsmap)
+		if item2in != nil {
+			assertf(bytes.Equal(value2, item2in.Value), "expected item with value %q, got %q", string(value2), string(item2in.Value))
+			assertf(item2in.Timestamps.Expiration == nil, "expected nil Expiration timestamp, got %v", item2in.Timestamps.Expiration)
+			assertf(item2in.Timestamps.LastAccess == nil, "expected nil LastAccess timestamp, got %v", item2in.Timestamps.LastAccess)
+		}
+
+		fmt.Fprintf(w, "\nmemcache.PeekMulti(ctx, %q)...\n", keys)
+		itemsmap, err = memcache.PeekMulti(ctx, keys)
+		assertf(err == nil, "expected nil error, got %v", err)
+		assertf(len(itemsmap) == len(keys), "expected %d entries, got a map with %d entries", len(keys), len(itemsmap))
+
+		item1in = itemsmap[key1]
+		assertf(item1in != nil, "entry %q not found in %v", key1, itemsmap)
+		if item1in != nil {
+			assertf(bytes.Equal(value1, item1in.Value), "expected item with value %q, got %q", string(value1), string(item1in.Value))
+			assertf(item1in.Timestamps.Expiration == nil, "expected nil Expiration timestamp, got %v", item1in.Timestamps.Expiration)
+			assertf(item1in.Timestamps.LastAccess != nil, "expected non-nil LastAccess timestamp")
+			fmt.Fprintf(w, "Item 1 Expiration timestamp: %v\n", item1in.Timestamps.Expiration)
+			fmt.Fprintf(w, "Item 1 LastAccess timestamp: %v\n", item1in.Timestamps.LastAccess)
+		}
+
+		item2in = itemsmap[key2]
+		assertf(item2in != nil, "entry %q not found in %v", key2, itemsmap)
+		if item2in != nil {
+			assertf(bytes.Equal(value2, item2in.Value), "expected item with value %q, got %q", string(value2), string(item2in.Value))
+			assertf(item2in.Timestamps.Expiration != nil, "expected non-nil Expiration timestamp")
+			if expi := item2in.Timestamps.Expiration; expi != nil {
+				// Expiration timestamp should be in more than 3mn and less than 6mn
+				d := time.Until(*expi)
+				assertf(d > 3*time.Minute, "Expiration timestamp is %v which is in %v, which is in less than 3mn", expi, d)
+				assertf(d < 6*time.Minute, "Expiration timestamp is %v which is in %v, which is in more than 6mn", expi, d)
+			}
+			assertf(item2in.Timestamps.LastAccess != nil, "expected non-nil LastAccess timestamp")
+			fmt.Fprintf(w, "Item 2 Expiration timestamp: %v\n", item2in.Timestamps.Expiration)
+			fmt.Fprintf(w, "Item 2 LastAccess timestamp: %v\n", item2in.Timestamps.LastAccess)
+		}
+	}
+
 	// Cleanup (teardown)
-	key := "mykey"
-	_ = memcache.Delete(ctx, key)
+	keys := []string{"mykey", "mykey1", "mykey2", "EMPTY"}
+	_ = memcache.DeleteMulti(ctx, keys)
 }
